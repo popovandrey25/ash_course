6ee0e633ed2525dbd5ab2239d651b1d3d6112ccb
Andrey Popov <palanelarty@gmail.com>
2024-09-12 11:58:07 +0300

    first steps

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def func():
+    return "Hello World!!!!!!!!!!"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
