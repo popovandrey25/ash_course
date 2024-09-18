860ea39ac37d052d65aa4cd7819be6e4a564d5be
Andrey Popov <palanelarty@gmail.com>
2024-09-18 14:08:02 +0300

    refactor code

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,37 +1,16 @@
-from datetime import datetime, timedelta, timezone
-from typing import Annotated
-
-import jwt
 from fastapi import APIRouter, HTTPException, Response
-from passlib.context import CryptContext
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix='/auth', tags=['Авторизация и аутентификация'])
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = '325h4h3k45k34k523v45gv32v45v2345h3452vk'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 
 @router.post('/register')
 async def register_user(data: UserRequestAdd):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().pwd_context.hash(data.password)
     new_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_data)
@@ -50,8 +29,10 @@ async def login_user(data: UserRequestAdd, response: Response):
                 status_code=401,
                 detail='Пользователь с таким мылом не зарегистрирован'
             )
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(
+            data.password, user.hashed_password
+        ):
             raise HTTPException(status_code=401, detail="Пароль неверный")
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
     return {"access_token": access_token}
